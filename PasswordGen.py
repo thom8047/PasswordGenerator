822af3f29731cc22739182ace728bf337d805666
thom8047 <thom8047@bears.unco.edu>
2021-01-11 23:08:23 -0700

    Updated all the work and README file.

---
+++ PasswordGen.py
@@ -0,0 +1,19 @@
+import random
+import winclip32
+
+def getPass(n):
+    characters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*?')
+    password = ''
+    for x in range(int(n)):
+        spot = random.randint(1, len(characters))
+        password += characters[spot]
+    
+    return password
+
+
+if __name__ == '__main__':
+    password = getPass(input('Length of password: '))
+
+    winclip32.set_clipboard_data("unicode_std_text", password)
+
+    print(winclip32.get_clipboard_data("unicode_std_text"))
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PasswordGenerator
+
+Run script via command shell. Put in an integer the size of the desired password. Automatically puts password in users Mouse ClipBoard Data to be pasted for secrecy and/or pasted for viewing. 
+
+Script randomly selects characters from characters: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*?"
